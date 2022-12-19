cb6cb3183d4c82ab9cc08ad97b184821c376399b
michelle <113802263+michelletrn@users.noreply.github.com>
2022-12-18 21:52:10 -0500

    files modified, readme generates with info inputted from user

--- index.js
+++ index.js
@@ -1,13 +1,73 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Please enter Github username. ',
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: 'Please enter contact email for questions/concerns. ',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'Enter project title.',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Provide a description of your project. (Please enter "N/A", if not applicable for your project.)',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Please provide installation instructions. (Please enter "N/A", if not applicable for your project.)',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Please enter any instructions for use. (Please enter "N/A", if not applicable for your project.)',
+        name: 'usage'
+    },
+    {
+        type: 'list',
+        message: 'Select your license',
+        name: 'license',
+        choices: ['MIT', 'APACHE 2.0', 'BSD 3', 'GPL 3.0', 'None']
+    },
+    {
+        type: 'input',
+        message: 'Please name contributor(s):',
+        name: 'contributor',
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    },
+    {
+        type: 'input',
+        message: 'How can others contribute to the project? (Please enter "N/A", if not applicable for your project.)',
+        name: 'contributeHow',
+    },
+    {
+        type: 'input',
+        message: 'Please describe project features here:  (Please enter "N/A", if not applicable for your project.)',
+        name: 'features'
+    },
+    {
+        type: 'input',
+        message: 'Please describe tests here:  (Please enter "N/A", if not applicable for your project.)',
+        name: 'tests'
+    }
 
-// TODO: Create a function to initialize app
-function init() {}
+];
 
-// Function call to initialize app
-init();
+inquirer.prompt(questions)
+    .then((data) => {
+        const readme = generateMarkdown(data);
+
+        fs.writeFile(`${data.title.toLowerCase().split(' ').join('')}-README.md`, readme, (err) =>
+            err ? consosle.log(err) : console.log('Generating README...'));
+    })
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,78 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+var badge;
+var badgeLink;
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(l) {
+  if (l.license == 'MIT') {
+    badge = `[![License: MIT](https://img.shields.io/badge/license-MIT-blue)](https://img.shields.io/badge/license-MIT-blue)`,
+      badgeLink = 'https://www.mit.edu/~amini/LICENSE.md'
+  }
+  else if (l.license == 'APACHE 2.0') {
+    badge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+      badgeLink = 'https://www.apache.org/licenses/LICENSE-2.0'
+  }
+  else if (l.license == 'BSD 3') {
+    badge = `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`,
+      badgeLink = 'https://opensource.org/licenses/BSD-3-Clause'
+  }
+  else if (l.license == 'GPL 3.0') {
+    badge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`,
+      badgeLink = 'https://www.gnu.org/licenses/gpl-3.0.en.html'
+  } else { badge = '', badgeLink = '' }
+  return badge, badgeLink
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  renderLicenseBadge(data);
 
-`;
+  return `# ${data.title} 
+   ${badge}
+
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents 
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributions](#contributions)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  
+  ## Installation
+  \n ${data.installation}
+  
+  
+  ## Usage
+  ${data.usage}
+ 
+ 
+  ## License
+  ${data.license}
+  ${badgeLink}
+  
+  ## Contributions
+  Contributors: ${data.contributor}
+  \n How to contribute: ${data.contributeHow}
+  
+  -----------------------------------------------------------------------------
+  
+  ## Features
+  
+  ${data.features}
+
+  ## Tests
+
+  ${data.tests}
+    
+  ## Questions
+  If you have any questions, please contact:
+  
+  Github: ${data.username}, 
+  \n Email: ${data.email}
+  `;
 }
 
 module.exports = generateMarkdown;
